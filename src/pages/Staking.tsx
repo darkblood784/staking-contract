63cd2e62bb48cacc66c9f984890ae2ce1ea43eee
darkblood784 <a.rouniyar144@gmail.com>
2024-09-26 14:16:49 +0800

    input css changed

--- src/pages/Staking.tsx
+++ src/pages/Staking.tsx
@@ -135,15 +135,15 @@ function Staking() {
                         </div>
                         <div className="flex mt-5 w-full justify-between text-white text-shadow-customp">
                             <div className="rounded-3xl border-gray-300 border w-[31%] h-auto text-center cursor-pointer" onClick={(e) => setUsdtDuration(t('day'))}>
-                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full">{t('day')}</div>
+                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full hover:opacity-40 active:opacity-50">{t('day')}</div>
                                 <p className=" text-[20px] md:text-[30px] my-auto hover:opacity-40 active:opacity-50">15%</p>
                             </div>
                             <div className="rounded-3xl border-gray-300 border w-[31%] h-auto text-center cursor-pointer" onClick={(e) => setUsdtDuration(t('month'))}>
-                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full">{t('month')}</div>
+                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full hover:opacity-40 active:opacity-50">{t('month')}</div>
                                 <p className=" text-[20px] md:text-[30px] my-auto hover:opacity-40 active:opacity-50">24%</p>
                             </div>
                             <div className="rounded-3xl border-gray-300 border w-[31%] h-auto text-center cursor-pointer" onClick={(e) => setUsdtDuration(t('year'))}>
-                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full">{t('year')}</div>
+                                <div className="bg-white text-black text-[15px] md:text-[17px] py-2 rounded-3xl md:rounded-full hover:opacity-40 active:opacity-50">{t('year')}</div>
                                 <p className=" text-[20px] md:text-[30px] my-auto hover:opacity-40 active:opacity-50">36%</p>
                             </div>
                         </div>
