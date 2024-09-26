8a445c81a0bd824c5e2ebac1e2e8317e3651a14f
darkblood784 <a.rouniyar144@gmail.com>
2024-09-26 15:42:39 +0800

    changed the staking page

--- src/pages/Staking.tsx
+++ src/pages/Staking.tsx
@@ -8,7 +8,7 @@ import ethbg from '../assets/ethereumplanbackground.png';
 import usdt from '../assets/usdt.png';
 import btc from '../assets/btc.png';
 import eth from '../assets/eth.png';
-import bg_whale from '../assets/bg-whale.png';
+import bg_whale from '../assets/Whale_Strategy.png';
 import linktree from '../assets/social/linktree.png';
 import discord from '../assets/social/discord.png';
 import symbol from '../assets/symbol.png';
