804574df6589cb64cb0f00330e7dd0d74142a918
darkblood784 <a.rouniyar144@gmail.com>
2024-09-26 11:29:31 +0800

    input css changed

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "../constants";
 import { WalletActionButton } from '@tronweb3/tronwallet-adapter-react-ui';
