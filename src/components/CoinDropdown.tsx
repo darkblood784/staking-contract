aabc1f15b6ffbfaeb830b38f47c1a32c32fba233
darkblood784 <a.rouniyar144@gmail.com>
2024-09-27 16:40:38 +0800

    changed the staking page

--- src/components/CoinDropdown.tsx
+++ src/components/CoinDropdown.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import usdtIcon from '../assets/usdt.png';
 import btcIcon from '../assets/btc.png';
 import ethIcon from '../assets/eth.png';
@@ -12,41 +11,52 @@ interface CoinDropdownProps {
 }
 
 const CoinDropdown: React.FC<CoinDropdownProps> = ({ selectedCoin, setSelectedCoin }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
     const coins = [
         { name: 'USDT', icon: usdtIcon },
         { name: 'BTC', icon: btcIcon },
         { name: 'ETH', icon: ethIcon },
     ];
 
-    return (
-        <div className="relative flex items-center space-x-2">
-            <div className="flex items-center">
-                {/* Dropdown field with icon inside */}
-                <select
-                    value={selectedCoin}
-                    onChange={(e) => setSelectedCoin(e.target.value as Coin)}
-                    className="appearance-none bg-transparent font-cubic text-[1.5vw] outline-none pr-6 flex items-center"
-                    style={{ color: '#5170fd' }}
-                >
-                    {coins.map((coin) => (
-                        <option key={coin.name} value={coin.name} className="bg-black text-white flex items-center">
-                            {coin.name}
-                        </option>
-                    ))}
-                </select>
+    const handleCoinSelect = (coin: Coin) => {
+        setSelectedCoin(coin);
+        setIsOpen(false); // Close the dropdown after selection
+    };
 
-                {/* Coin Icon */}
+    return (
+        <div className="relative inline-block text-left">
+            {/* Trigger to open/close the dropdown */}
+            <button
+                onClick={() => setIsOpen(!isOpen)}
+                className="flex items-center font-cubic text-[1.5vw] bg-transparent border-none outline-none"
+            >
                 <img
                     src={coins.find(coin => coin.name === selectedCoin)?.icon}
                     alt={selectedCoin}
-                    className="w-5 h-5 ml-2"
+                    className="w-5 h-5 mr-2"
                 />
-            </div>
+                {selectedCoin}
+                <span className="ml-2" style={{ color: '#5170fd', fontSize: '1.2vw' }}>
+                    &#x25BC;
+                </span>
+            </button>
 
-            {/* Smaller dropdown arrow */}
-            <span className="ml-1" style={{ color: '#5170fd', fontSize: '1.2vw' }}>
-                &#x25BC;
-            </span>
+            {/* Dropdown menu */}
+            {isOpen && (
+                <div className="absolute right-0 mt-2 w-40 bg-black text-white rounded shadow-lg z-10">
+                    {coins.map((coin) => (
+                        <div
+                            key={coin.name}
+                            className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-700"
+                            onClick={() => handleCoinSelect(coin.name as Coin)}
+                        >
+                            <img src={coin.icon} alt={coin.name} className="w-5 h-5 mr-2" />
+                            {coin.name}
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 };
