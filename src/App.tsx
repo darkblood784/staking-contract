1ef9e2065870f2b47ace4c3614b6cf5b48817946
darkblood784 <a.rouniyar144@gmail.com>
2024-10-11 17:55:56 +0800

    changed the staking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,37 @@
-// App.tsx
-
 import React, { useMemo, useState } from 'react';
+import type { Adapter, WalletError } from '@tronweb3/tronwallet-abstract-adapter';
+import { WalletDisconnectedError, WalletNotFoundError } from '@tronweb3/tronwallet-abstract-adapter';
 import { WalletProvider } from '@tronweb3/tronwallet-adapter-react-hooks';
-import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui';
+import {
+    WalletModalProvider,
+} from '@tronweb3/tronwallet-adapter-react-ui';
 import toast from 'react-hot-toast';
-import { TronLinkAdapter, WalletConnectAdapter, LedgerAdapter, TokenPocketAdapter, BitKeepAdapter, OkxWalletAdapter } from '@tronweb3/tronwallet-adapters';
+import { TronLinkAdapter, TokenPocketAdapter, BitKeepAdapter, OkxWalletAdapter } from '@tronweb3/tronwallet-adapters';
+import { WalletConnectAdapter } from '@tronweb3/tronwallet-adapter-walletconnect';
+import { LedgerAdapter } from '@tronweb3/tronwallet-adapter-ledger';
+
 import Navbar from './pages/Navbar';
 import Staking from './pages/Staking';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import detectEthereumProvider from '@metamask/detect-provider';
 
-declare global {
-  interface Window {
-    ethereum: any;
-  }
-}
 
 export function App() {
-    const [account, setAccount] = useState<string | null>(null);
-
-    async function connectMetaMask() {
-        const provider = await detectEthereumProvider();
-        if (provider) {
-            try {
-                const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-                setAccount(accounts[0]);
-            } catch (error) {
-                console.error(error);
-            }
-        } else {
-            console.log('Please install MetaMask');
-        }
-    }
-
-    function disconnectMetaMask() {
-        setAccount(null);
-    }
-
-    function onError(e: any) {
+    function onError(e: WalletError) {
         console.log(e);
-        toast.error(e.message);
+        if (e instanceof WalletNotFoundError) {
+            toast.error(e.message);
+        } else if (e instanceof WalletDisconnectedError) {
+            toast.error(e.message);
+        } else toast.error(e.message);
     }
-
     const adapters = useMemo(function () {
         const tronLink1 = new TronLinkAdapter();
         const walletConnect1 = new WalletConnectAdapter({
             network: 'Nile',
             options: {
                 relayUrl: 'wss://relay.walletconnect.com',
-                projectId: 'your-project-id',
+                // example WC app project ID
+                projectId: '5fc507d8fc7ae913fff0b8071c7df231',
                 metadata: {
                     name: 'Test DApp',
                     description: 'JustLend WalletConnect',
@@ -56,6 +39,19 @@ export function App() {
                     icons: ['https://your-dapp-url.org/mainLogo.svg'],
                 },
             },
+            web3ModalConfig: {
+                themeMode: 'dark',
+                themeVariables: {
+                    '--w3m-z-index': '1000'
+                },
+                // explorerRecommendedWalletIds: 'NONE',
+                enableExplorer: true,
+                explorerRecommendedWalletIds: [
+                    '225affb176778569276e484e1b92637ad061b01e13a048b35a9d280c3b58970f',
+                    '1ae92b26df02f0abca6304df07debccd18262fdf5fe82daa81593582dac9a369',
+                    '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
+                ]
+            }
         });
         const ledger = new LedgerAdapter({
             accountNumber: 2,
@@ -65,16 +61,20 @@ export function App() {
         const okxWalletAdapter = new OkxWalletAdapter();
         return [tronLink1, walletConnect1, ledger, tokenPocket, bitKeep, okxWalletAdapter];
     }, []);
-
+    function onConnect() {
+        console.log('onConnect');
+    }
+    async function onAccountsChanged() {
+        console.log('onAccountsChanged')
+    }
+    async function onAdapterChanged(adapter: Adapter | null) {
+        console.log('onAdapterChanged', adapter)
+    }
     return (
-        <WalletProvider onError={onError} adapters={adapters}>
+        <WalletProvider onError={onError} onConnect={onConnect} onAccountsChanged={onAccountsChanged} onAdapterChanged={onAdapterChanged} autoConnect={true} adapters={adapters} disableAutoConnectOnLoad={true}>
             <WalletModalProvider>
                 <div className="flex items-center flex-col bg-[#000000] text-stone-900 dark:text-stone-300 min-h-screen font-inter">
-                    <Navbar 
-                        connectMetaMask={connectMetaMask} 
-                        disconnectMetaMask={disconnectMetaMask} 
-                        account={account} 
-                    />
+                    <Navbar />
                     <div className="px-4 w-full lg:w-3/4 ">
                         <Staking />
                     </div>
--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,29 +1,27 @@
-// Navbar.tsx
-
 import { useState, useEffect } from 'react';
 import { useTranslation } from "react-i18next";
+import { LANGUAGES } from "../constants";
 import { WalletActionButton } from '@tronweb3/tronwallet-adapter-react-ui';
+
 import '../index.css';
+
 import '../custom.css';
 
-// Define props for Navbar
-interface NavbarProps {
-    connectMetaMask: () => Promise<void>;
-    disconnectMetaMask: () => void;
-    account: string | null;
-}
 
-function Navbar({ connectMetaMask, disconnectMetaMask, account }: NavbarProps) {
+function Navbar() {
     const { t, i18n } = useTranslation();
 
+    // Dynamically assign the font class based on the language
     const fontClass = i18n.language === 'en' ? 'font-cubic' : 'font-cubic';
     const [isOpen, setIsOpen] = useState(false);
 
+    // Initialize state for selected language label and image
     const [selectedLanguage, setSelectedLanguage] = useState({
-        label: t('English'),  
-        img: '/src/assets/language/en.svg'  
+        label: t('English'),  // Default language label
+        img: '/src/assets/language/en.svg'  // Default image path
     });
 
+    // Set the correct language and flag on component mount
     useEffect(() => {
         const currentLang = i18n.language || 'en';
         const selectedLang = LANGUAGES.find(({ code }) => code === currentLang);
@@ -31,44 +29,30 @@ function Navbar({ connectMetaMask, disconnectMetaMask, account }: NavbarProps) {
         if (selectedLang) {
             setSelectedLanguage({
                 label: selectedLang.label,
-                img: selectedLang.lang,
+                img: selectedLang.lang, // Path to the correct flag
             });
         }
     }, [i18n.language]);
 
     const onChangeLang = (code: string, label: string, img: string) => {
         i18n.changeLanguage(code);
-        setSelectedLanguage({ label, img });
+        setSelectedLanguage({ label, img });  // Update the label and image
         setIsOpen(false);
-    };
+    }
 
     return (
         <div className={`px-2 flex w-full items-center justify-between fixed z-40 top-0 left-0 h-28 md:pr-8 dark:bg-[rgba(255,255,255,0)] backdrop-blur-[30px] shadow-[0_3px_6px_3px_rgba(0,0,0,0.4)] transition-all duration-300 ${fontClass}`}>
+            {/* Left side with Logo */}
             <div className="flex items-center">
                 <a href="https://staking.whalestrategy.net/">
                     <img src="/logo.png" className="w-16 h-16 sm:ml-10" alt="Logo" />
                 </a>
             </div>
 
+            {/* Right side with Connect Wallet and Language Selector */}
             <div className="flex items-center">
                 <WalletActionButton className={`w-28 md:w-32 flex justify-center bg-[#5170fd] text-white text-[16px] font-bold hover:scale-105 transition-transform duration-300 ${fontClass}`} />
                 
-                {account ? (
-                    <button
-                        onClick={disconnectMetaMask}
-                        className="w-28 md:w-32 bg-red-500 text-white rounded"
-                    >
-                        Disconnect: {account.slice(0, 6)}...{account.slice(-4)}
-                    </button>
-                ) : (
-                    <button
-                        onClick={connectMetaMask}
-                        className="w-28 md:w-32 bg-green-500 text-white rounded"
-                    >
-                        Connect MetaMask
-                    </button>
-                )}
-
                 <div className="relative">
                     <button onClick={() => setIsOpen(!isOpen)} className="flex items-center justify-center w-24 md:w-32 h-12 rounded-[4px] p-2 bg-[#5170fd] text-white text-[16px] font-bold ml-4 hover:scale-105 transition-transform duration-300 ">
                         {selectedLanguage.img && (
