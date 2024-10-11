676ab5a034fe7451bceec2f62561929cdd95b997
darkblood784 <a.rouniyar144@gmail.com>
2024-10-11 17:42:37 +0800

    changed the staking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
+// App.tsx
+
 import React, { useMemo, useState } from 'react';
-import type { Adapter, WalletError } from '@tronweb3/tronwallet-abstract-adapter';
-import { WalletDisconnectedError, WalletNotFoundError } from '@tronweb3/tronwallet-abstract-adapter';
 import { WalletProvider } from '@tronweb3/tronwallet-adapter-react-hooks';
-import {
-    WalletModalProvider,
-} from '@tronweb3/tronwallet-adapter-react-ui';
+import { WalletModalProvider } from '@tronweb3/tronwallet-adapter-react-ui';
 import toast from 'react-hot-toast';
-import { TronLinkAdapter, TokenPocketAdapter, BitKeepAdapter, OkxWalletAdapter } from '@tronweb3/tronwallet-adapters';
-import { WalletConnectAdapter } from '@tronweb3/tronwallet-adapter-walletconnect';
-import { LedgerAdapter } from '@tronweb3/tronwallet-adapter-ledger';
+import { TronLinkAdapter, WalletConnectAdapter, LedgerAdapter, TokenPocketAdapter, BitKeepAdapter, OkxWalletAdapter } from '@tronweb3/tronwallet-adapters';
 import Navbar from './pages/Navbar';
 import Staking from './pages/Staking';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -22,7 +18,7 @@ declare global {
 
 export function App() {
     const [account, setAccount] = useState<string | null>(null);
-    
+
     async function connectMetaMask() {
         const provider = await detectEthereumProvider();
         if (provider) {
@@ -41,13 +37,9 @@ export function App() {
         setAccount(null);
     }
 
-    function onError(e: WalletError) {
+    function onError(e: any) {
         console.log(e);
-        if (e instanceof WalletNotFoundError) {
-            toast.error(e.message);
-        } else if (e instanceof WalletDisconnectedError) {
-            toast.error(e.message);
-        } else toast.error(e.message);
+        toast.error(e.message);
     }
 
     const adapters = useMemo(function () {
@@ -79,9 +71,9 @@ export function App() {
             <WalletModalProvider>
                 <div className="flex items-center flex-col bg-[#000000] text-stone-900 dark:text-stone-300 min-h-screen font-inter">
                     <Navbar 
-                        connectMetaMask={connectMetaMask}
-                        disconnectMetaMask={disconnectMetaMask}
-                        account={account}
+                        connectMetaMask={connectMetaMask} 
+                        disconnectMetaMask={disconnectMetaMask} 
+                        account={account} 
                     />
                     <div className="px-4 w-full lg:w-3/4 ">
                         <Staking />
